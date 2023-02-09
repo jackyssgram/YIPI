0085f01f504aa919d5bb8f328862bbc80f69d1f5
jackyssgram <117629733+jackyssgram@users.noreply.github.com>
2023-02-09 18:32:22 +0800

    Add files via upload

--- news/NewsId.jsx
+++ news/NewsId.jsx
@@ -1,84 +1,155 @@
-import React from 'react'
-import './newsId.css'
-import { FaHeart } from 'react-icons/fa'
-import { FiShare } from 'react-icons/fi'
-import { IoIosTimer } from 'react-icons/io'
-import { FaMapMarker } from 'react-icons/fa'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import { Link } from 'react-router-dom'
+// import React from 'react'
+import React, { useState } from "react";
+import "./newsId.css";
+import { FaHeart } from "react-icons/fa";
+import { FiShare } from "react-icons/fi";
+import { IoIosTimer } from "react-icons/io";
+import { FaMapMarker } from "react-icons/fa";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const NewsId = () => (
-  <div>
-    <div className="container">
-      <div className="wrapper">
-        <img className="yipai__img" src={require('./first.jpg')} alt="banner" />
-        <div>
-          <div className="d-flex flex-row justify-content-center my-4 a">
-            <div className="yipai_newsId">
-              <div className="icon">
-                <box className="collect">
-                  <Link to="#">
-                    <p>
-                      <FaHeart className="FaHeart_icon" />
-                      收藏
-                    </p>
-                  </Link>
-                </box>
-                <box className="share">
-                  <Link to="#">
-                    <p>
-                      {' '}
-                      <FiShare className="share_icon" />
-                      分享
-                    </p>
-                  </Link>
-                </box>
-              </div>
-              <h1 className="fw-bold">達文西光影藝術特展</h1>
-              <div className="d-flex flex-row justify-content-center my-4 ">
-                <IoIosTimer className="timer_icon" />
-                2022-12-01 ~ 2022-12-07
+//地圖
+import {
+  MapContainer as LeafletMap,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { margin } from "@mui/system";
+
+const NewsId = () => {
+  const { newsId } = useParams();
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    // console.log('第二個參數是空陣列')
+    // 在 component 初始化的時候跑一次
+    // 通常會把去跟後端要資料的動作放在這裡
+
+    async function getdata() {
+      let response = await axios.get(`http://localhost:3001/news/${newsId}?`);
+      setdata(response.data);
+      // console.log(response.data)
+    }
+    getdata();
+  }, []);
+
+  const customMarker = new L.Icon({
+    iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
+    // iconSize: [25, 41],
+    // iconAnchor: [10, 41],
+    // popupAnchor: [20, -40],
+  });
+
+  return (
+    <div>
+      <div className="news__container">
+        {data.map((newsDetail) => {
+          return (
+            <div className="wrapper">
+              <img
+                className="news__yipai__img"
+                src={newsDetail.img_file}
+                alt="banner"
+              />
+              <div>
+                <div className="d-flex flex-row justify-content-center my-4 news__a">
+                  <div className="yipai_newsId">
+                    <div className="news__timericon">
+                      <box className="news__collect">
+                        <Link to="#">
+                          <p>
+                            <FaHeart className="news__FaHeart_icon" />
+                            收藏
+                          </p>
+                        </Link>
+                      </box>
+                      <box className="news__share">
+                        <Link to="#">
+                          <p>
+                            {" "}
+                            <FiShare className="news__share_icon" />
+                            分享
+                          </p>
+                        </Link>
+                      </box>
+                    </div>
+                    <div key={newsDetail.id}>
+                      <h1 className="news__fw-bold">{newsDetail.name}</h1>
+                      <div className="d-flex flex-row justify-content-center my-2 ">
+                        <IoIosTimer className="news__timer_icon" />
+                        {newsDetail.news_time}
+                      </div>
+                    </div>
+                    <Container>
+                      <Row className="news__row">
+                       
+                          <p className="newsDetail__text" align="left">
+                            {newsDetail.news_introduction}
+                          </p>
+                      
+                        <div className="newsDetail_address d-flex flex-row justify-content-center ">
+                          <h5 className="newsDetail_address-icon">
+                            {" "}
+                            <FaMapMarker />
+                            地點
+                          </h5>
+                          <h5> {newsDetail.news_address}</h5>
+                        </div>
+
+                        <div className="news__google">
+                          <LeafletMap
+                            center={[
+                              newsDetail.news_lat,
+                              newsDetail.news_lng,
+                            ]}
+                            zoom={22}
+                            style={{ height: "50vh", width: "100%", margin: "auto" }}
+                          >
+                          
+                                  
+                            <TileLayer
+                              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                              url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                            />
+                            {/* {state.map(({ lat, lng }, index) => ( */}
+                            <Marker
+                              position={[
+                                newsDetail.news_lat,
+                                newsDetail.news_lng,
+                              ]}
+                              icon={customMarker}
+                              // key={index}
+                            >
+                              {/* <Popup>
+                            {index + 1} is for popup with lat: {lat} and lon {lng}
+                              </Popup> */}
+                            </Marker>
+                            {/* ))} */}
+                             </LeafletMap>
+                            {/* <img
+                            className="map__img"
+                            src={require("./Map.jpg")}
+                          /> */}
+                        </div>
+                      </Row>
+                    </Container>
+                  </div>
+                </div>
               </div>
             </div>
-            <Container>
-              <Row>
-                <Col>
-                  <h6 className="news__text" align="left">
-                    全台首次以達文西為主題的藝術光影特展《走進大師的腦內：李奧納多·達文西-光影體驗展》，將於
-                    12 月 2 日在双融域 AMBI SPACE
-                    ONE（台北101五樓）作為亞洲首站展出， 此活動會有一面 mini
-                    gallery，將展出與達文西相關的創作，主辦單位特別給 The
-                    Bridege Art 的會員藝術家 3
-                    位名額可以展出作品與販售週邊全台首次以達文西為主題的藝術光影特展《走進大師的腦內：李奧納多·達文西-光影體驗展》，將於
-                    12 月 2 日在双融域 AMBI SPACE
-                    ONE（台北101五樓）作為亞洲首站展出，此活動會有一面 mini
-                    gallery，將展出與達文西相關的創作，主辦單位特別給 The
-                    Bridege Art 的會員藝術家 3 位名額可以展出作品與販售週邊!
-                  </h6>
-                </Col>
-                <div className="address">
-                  <box className=" fw-bold">
-                    <FaMapMarker />
-                    地點
-                    <h6 >双融域 AMBI SPACE ONE（台北101五樓）</h6>
-                  </box>
-                  
-                </div>
-
-                <Col>
-                  <div className="mapC">
-                    <img className="map__img" src={require('./Map.jpg')} />
-                  </div>
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
-  </div>
-)
+  );
+};
 
-export default NewsId
+export default NewsId;
